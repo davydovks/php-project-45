2da5870e27ce31b09b936abe75cbd74592c492cb
Konstantin Davydov <davydovks@mail.ru>
2023-02-18 13:15:26 +0500

    add Brain-progression game source code

---
+++ src/Games/Brain-progression.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BrainGames\Games\Brain\Progression;
+
+use function BrainGames\Engine\runGame;
+
+function getQnAForProgression($count)
+// Возвращает вопросы и правильные ответы для игры "Арифметическая прогрессия"  
+// в формате массива из двух массивов: в первом массиве вопросы (задания), во
+// втором - правильные ответы на них. Аргумент $count определяет количество
+// элементов в обоих массивах (количество задаваемых вопросов).
+{
+    $questions = [];
+    $answers = [];
+    for ($i = 0; $i < $count; $i++) {
+        $start = rand(1, 20);
+        $increment = rand(1, 9);
+        $position = rand(1, 8); // Скрываем число между 2 и 9 включительно (из 10)
+
+        $question = '';
+        for ($j = 0, $number = $start; $j < 10; $j++, $number += $increment) {
+            if ($j == $position) {
+                // Скрываем загаданное число
+                $question .= '.. ';
+                $answers[] = $number;
+            } else {
+                // Добавляем число в строку
+                $question .= $number . ' ';
+            }
+        }
+        $questions[] = $question;
+    }
+
+    return [$questions, $answers];
+}
+
+function runProgression($count = 3)
+// Запускает игру "Арифметическая прогрессия"
+// $count - количество раундов
+{
+    $task = 'What number is missing in the progression?';
+    $QnA = getQnAForProgression($count);
+    runGame($task, $QnA, $count);
+}
