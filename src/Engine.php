09ad544543e51f09c4057e06c76128bfaa0f7969
Konstantin Davydov <davydovks@mail.ru>
2023-03-05 19:12:14 +0500

    eliminate excessive functions askOneQuestion and farewell in Engine

--- src/Engine.php
+++ src/Engine.php
@@ -6,37 +6,22 @@ use function cli\line;
 use function cli\prompt;
 use function BrainGames\Cli\askName;
 
-function askOneQuestion(string $question, string $answer): bool
-{
-    line('Question: %s', $question);
-    $userAnswer = prompt("Your answer");
-    if ($userAnswer == $answer) {
-        line("Correct!");
-        return true;
-    } else {
-        line("'$userAnswer' is wrong answer ;(. Correct answer was '$answer'.");
-        return false;
-    }
-}
-
-function askQuestions(array $questions, array $answers, int $count): bool
+function askQuestions(array $questions, array $answers, int $count, string $name)
 {
     for ($i = 0; $i < $count; $i++) {
-        $result = askOneQuestion($questions[$i], $answers[$i]);
-        if (!$result) {
+        line('Question: %s', $questions[$i]);
+        $userAnswer = prompt("Your answer");
+        if ($userAnswer == $answers[$i]) {
+            line("Correct!");
+        } else {
+            line("'$userAnswer' is wrong answer ;(. Correct answer was '{$answers[$i]}'.");
+            line("Let's try again, %s!", $name);
             return false;
         }
     }
-    return true;
-}
 
-function farewell(string $name, bool $success)
-{
-    if ($success) {
-        line("Congratulations, %s!", $name);
-    } else {
-        line("Let's try again, %s!", $name);
-    }
+    line("Congratulations, %s!", $name);
+    return true;
 }
 
 function runGame(string $task, array $QnA, int $count)
@@ -44,6 +29,5 @@ function runGame(string $task, array $QnA, int $count)
     $username = askName();
     line($task);
     [$questions, $answers] = $QnA;
-    $result = askQuestions($questions, $answers, $count);
-    farewell($username, $result);
+    askQuestions($questions, $answers, $count, $username);
 }
