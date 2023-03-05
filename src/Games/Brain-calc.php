a763a6a39615b5c43115f7408d7fb09484c5f39d
Konstantin Davydov <davydovks@mail.ru>
2023-03-05 21:33:14 +0500

    move switch in Brain-calc into a separate function

--- src/Games/Brain-calc.php
+++ src/Games/Brain-calc.php
@@ -6,36 +6,43 @@ use function BrainGames\Engine\runGame;
 
 const TASK = 'What is the result of the expression?';
 
+function getQnA(int $num1, int $num2, string $operation): array
+{
+    switch ($operation) {
+        case '+':
+            $question = $num1 . ' + ' . $num2;
+            $answer = $num1 + $num2;
+            break;
+        case '*':
+            $question = $num1 . ' * ' . $num2;
+            $answer = $num1 * $num2;
+            break;
+        case '-':
+            if ($num1 < $num2) {
+                $temp = $num1;
+                $num1 = $num2;
+                $num2 = $temp;
+            }
+            $question = $num1 . ' - ' . $num2;
+            $answer = $num1 - $num2;
+            break;
+        default:
+            throw new \Exception("Unknown operation: '${operation}'!");
+    }
+
+    return [$question, $answer];
+}
+
 function getQnAForCalc(int $count): array
 {
     $operations = ['+', '*', '-'];
     $questions = [];
     $answers = [];
     for ($i = 0; $i < $count; $i++) {
-        $number1 = rand(1, 30);
-        $number2 = rand(1, 30);
+        $num1 = rand(1, 30);
+        $num2 = rand(1, 30);
         $operation = $operations[rand(0, 2)];
-        switch ($operation) {
-            case '+':
-                $questions[] = $number1 . ' + ' . $number2;
-                $answers[] = $number1 + $number2;
-                break;
-            case '*':
-                $questions[] = $number1 . ' * ' . $number2;
-                $answers[] = $number1 * $number2;
-                break;
-            case '-':
-                if ($number1 < $number2) {
-                    $temp = $number1;
-                    $number1 = $number2;
-                    $number2 = $temp;
-                }
-                $questions[] = $number1 . ' - ' . $number2;
-                $answers[] = $number1 - $number2;
-                break;
-            default:
-                throw new \Exception("Unknown operation: '${operation}'!");
-        }
+        [$questions[], $answers[]] = getQnA($num1, $num2, $operation);
     }
 
     return [$questions, $answers];
