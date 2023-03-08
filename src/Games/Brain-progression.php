35db2de84a5ec98fe6d37254b71b32e790a878b0
Konstantin Davydov <davydovks@mail.ru>
2023-03-08 08:54:16 +0500

    add PROGRESSION_COUNT constant

--- src/Games/Brain-progression.php
+++ src/Games/Brain-progression.php
@@ -7,6 +7,7 @@ use function BrainGames\Engine\runGame;
 use const BrainGames\Engine\ROUND_COUNT;
 
 const TASK = 'What number is missing in the progression?';
+const PROGRESSION_COUNT = 10;
 
 function runProgression(int $count = ROUND_COUNT)
 {
@@ -16,8 +17,8 @@ function runProgression(int $count = ROUND_COUNT)
     for ($i = 0; $i < $count; $i++) {
         $start = rand(1, 20);
         $increment = rand(1, 9);
-        $position = rand(1, 8); // Скрываем число между 2 и 9 включительно (из 10)
-        $end = $start + $increment * 10;
+        $position = rand(1, PROGRESSION_COUNT - 2); // Скрываем число между 2 и предпоследним
+        $end = $start + $increment * PROGRESSION_COUNT;
         $progression = range($start, $end, $increment);
         $progression[$position] = '..';        
         $questions[] = implode(' ', $progression);
